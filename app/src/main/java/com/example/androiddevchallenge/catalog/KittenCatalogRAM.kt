0e7fb0c62c1aefe0b1608fe00bcdca04794f0502
Frédéric Torcheux <frederic.torcheux@djit.fr>
2021-03-02 20:24:00 +0100

    [Catalog] Add new cats

--- app/src/main/java/com/example/androiddevchallenge/catalog/KittenCatalogRAM.kt
+++ app/src/main/java/com/example/androiddevchallenge/catalog/KittenCatalogRAM.kt
@@ -33,6 +33,54 @@ class KittenCatalogRAM : KittenCatalog {
                 listOf("Gray", "Pink"),
                 "european",
                 "22 cité des cèdres, Quiberon, France"
+            ),
+            Kitten(
+                "kitten_id_3",
+                "Rocket",
+                "rocket",
+                "A small version of cat with a giant heart, who will give you all the love she has to you!",
+                KittenGender.MALE,
+                KittenAge(0, 8),
+                KittenSize.NORMAL,
+                listOf("Gray", "White"),
+                "european",
+                "22 cité des cèdres, Quiberon, France"
+            ),
+            Kitten(
+                "kitten_id_4",
+                "Misa",
+                "misa",
+                "A small version of cat with a giant heart, who will give you all the love she has to you!",
+                KittenGender.FEMALE,
+                KittenAge(5, 0),
+                KittenSize.NORMAL,
+                listOf("Black", "Brown", "Beige"),
+                "european",
+                "22 cité des cèdres, Quiberon, France"
+            ),
+            Kitten(
+                "kitten_id_5",
+                "Eugene",
+                "eugene",
+                "A small version of cat with a giant heart, who will give you all the love she has to you!",
+                KittenGender.MALE,
+                KittenAge(8, 0),
+                KittenSize.NORMAL,
+                listOf("Ginger"),
+                "european",
+                "22 cité des cèdres, Quiberon, France"
+            ),
+            Kitten(
+                "kitten_id_6",
+                "Moustache",
+                "chat_lea",
+                "A small version of cat with a giant heart, who will give you all the love she has to you!",
+                KittenGender.FEMALE,
+                KittenAge(10, 0),
+                KittenSize.NORMAL,
+                listOf("Black", "White"),
+                "european",
+                "22 cité des cèdres, Quiberon, France"
             )
         )
     }
